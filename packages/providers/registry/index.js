4e19f1cbc08fc8bfa97fb0922ccd9d94d51f63e2
Nguyễn Hữu Quý <22485887+nguyenquy0710@users.noreply.github.com>
2026-06-27 13:36:52 +0700

    feat: add sambanova to packages/registry index

---
+++ packages/providers/registry/index.js
@@ -0,0 +1,11 @@
+// Auto-generated: static imports of all registry entries
+import pc01 from "./kira.js";
+import pc02 from "./llm7.js";
+import pc03 from "./sambanova.js";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  pc01,
+  pc02,
+  pc03
+];
