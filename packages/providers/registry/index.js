fbcd4f54682d53d2b7d5fe0a8d0cd12861f386ec
nguyenquy0710 <nguyenquy.1096@gmail.com>
2026-07-29 20:24:02 +0700

    feat(providers): add Vilao AI provider and update registry imports

--- packages/providers/registry/index.js
+++ packages/providers/registry/index.js
@@ -3,11 +3,13 @@ import pc01 from "./kira.js";
 import pc02 from "./llm7.js";
 import pc03 from "./sambanova.js";
 import pc04 from "./revidapi.js";
+import pc05 from "./vilao.js";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
   pc01,
   pc02,
   pc03,
-  pc04
+  pc04,
+  pc05,
 ];
