abbe9a5bb609f3ab2bc6d48fbf4a32290cd0afd8
nguyenquy0710 <nguyenquy.1096@gmail.com>
2026-08-27 21:20:17 +0700

    feat: add Meta LLM provider with configuration and pricing details

--- packages/providers/registry/index.js
+++ packages/providers/registry/index.js
@@ -10,6 +10,7 @@ import pc06 from "./codely.js";
 import pc07 from "./zenmux.js";
 import pc08 from "./opencode-zen.js";
 import pc09 from "./aimlapi.js";
+import pc10 from "./meta-llm.js";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
@@ -22,4 +23,5 @@ export default [
   pc07,
   pc08,
   pc09,
+  pc10,
 ];
